4db8b20d90d53ea7b259bb73329f9ccfffa04eaf
Enrique Bermúdez <enrique.bermudez@mercadolibre.com>
2021-12-02 23:52:24 -0300

    feat: add find_one method to base_model

--- db/base_model.py
+++ db/base_model.py
@@ -4,6 +4,11 @@ class BaseModel:
         self.db = db
         self.collection = self.db[self.name]
 
+
     def insert_one(self, data):
         insert_id = self.collection.insert_one(data).inserted_id
         return insert_id
+
+
+    def find_one(self, query):
+        return self.collection.find_one(query)
